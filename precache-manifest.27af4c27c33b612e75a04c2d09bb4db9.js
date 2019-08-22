c270544be69876bd94f1ccfa75d31c041b09b5b2
Ahmet Kuslular <ahmet.kuslular@iyzico.com>
2019-08-22 13:33:40 +0300

    Updates

---
+++ precache-manifest.27af4c27c33b612e75a04c2d09bb4db9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acc61f306a64712315080e6a0566c4a5",
+    "url": "/react-expand-collapse-example/index.html"
+  },
+  {
+    "revision": "4893124e18cb659e4e0e",
+    "url": "/react-expand-collapse-example/static/css/main.9cc1f00d.chunk.css"
+  },
+  {
+    "revision": "82e331bb32a7a501ca6f",
+    "url": "/react-expand-collapse-example/static/js/2.572b4746.chunk.js"
+  },
+  {
+    "revision": "4893124e18cb659e4e0e",
+    "url": "/react-expand-collapse-example/static/js/main.00be6036.chunk.js"
+  },
+  {
+    "revision": "87def517da1411ba64e7",
+    "url": "/react-expand-collapse-example/static/js/runtime~main.a6fc9951.js"
+  }
+]);
